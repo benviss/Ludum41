68c3ed64685147dfc230fb5aa07ed6e9defaf91f
Ben vissotzky <ben.vissotzky@gmail.com>
2018-04-21 09:17:03 -0700

    init repo commit

---
+++ Ludum41/Assets/Scripts/GameControl/Managers/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager:Singleton<AudioManager> {
+
+  public bool isMuted = true;
+
+  [SerializeField]
+  private AudioClip music, death, coin, jump;
+
+  public AudioSource musicSource;
+  public AudioSource sfxSource;
+
+  private void Start() {
+    if(!isMuted) {
+      PlayMusic();
+    }
+  }
+
+  public void ToggleMusic() {
+    isMuted = !isMuted;
+  }
+
+  void PlayMusic() {
+    if(isMuted) return;
+    if(musicSource.isActiveAndEnabled && !musicSource.isPlaying)
+      musicSource.clip = music;
+    musicSource.Play();
+  }
+
+  public void PlayDeathSound() {
+    PlayClip(death);
+  }
+
+  public void PlayCoinSound() {
+    PlayClip(coin);
+  }
+
+  public void PlayJumpSound() {
+    PlayClip(jump);
+  }
+
+  void PlayClip(AudioClip clip) {
+    if(isMuted) return;
+    if(musicSource.isActiveAndEnabled && !sfxSource.isPlaying) {
+      sfxSource.PlayOneShot(clip);
+    }
+  }
+}
---
+++ Ludum41/Assets/Scripts/GameControl/Managers/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioManager))]
+public class GameManager: Singleton<GameManager> {
+
+
+  public float coinSlowPercentage = .1f;
+  public float coinsCollected = 0;
+  public float lastSpikeTime = 0;
+  public float minSpikeInterval = .5f;
+
+  Player player;
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  private void Awake() {
+
+  }
+  private void Start() {
+    player = FindObjectOfType<Player>();
+  }
+
+}
