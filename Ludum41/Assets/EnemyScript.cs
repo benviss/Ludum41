0450859d2fa0f8c70a3094d0cf2620a628d50665
Spencer Fisher <Fisher.spencer@gmail.com>
2018-04-21 11:46:48 -0700

    Enemy spawners

--- Ludum41/Assets/EnemyScript.cs
+++ Ludum41/Assets/EnemyScript.cs
@@ -4,48 +4,59 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Renderer))]
 public class EnemyScript : MonoBehaviour {
 
     NavMeshAgent pathfinder;
     Transform player;
-    Vector3 target;
+    public Vector3 target;
     Material myMat;
     public bool isFleeing = false;
     public float range = 0;
+
+    float updatePathTime = .1f;
+    float lastUpdateTime = 0;
 
 
 	// Use this for initialization
 	void Start () {
         pathfinder = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        myMat = GetComponent<Material>();
+        myMat = GetComponent<Renderer>().material;
         target = transform.position;
 	}
 
     // Update is called once per frame
     void Update()
     {
-        UpdateTarget();
-        pathfinder.SetDestination(target);
+        if (lastUpdateTime + updatePathTime < Time.time)
+        {
+            lastUpdateTime = Time.time;
+            UpdateTarget();
+            pathfinder.SetDestination(target);
+        }
+
+        // Draw Target Loc
+        Debug.DrawLine(transform.position, target, Color.blue);
     }
 
     void UpdateTarget()
     {
+        Vector3 direction = transform.position - player.position;
         if (isFleeing)
         {
             // RUNNN AWAYY
-            target = (transform.position - player.position) *2;
+            target = transform.position + direction.normalized * 5;
         }
         else
         {
-            if ((transform.position - player.position).magnitude > range)
-            {
-                target = Vector3.Lerp(transform.position, player.position, .8f);
-            }
-            else
+            target = player.position + direction.normalized * range;
+
+            if (direction.magnitude < range + .1)
             {
                 // shoot
-                myMat.color = Color.red;
+                myMat.color = Color.blue;
+                isFleeing = true;
             }
         }
     }
---
+++ Ludum41/Assets/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    List<GameObject> enemies;
+    float nextSpawnTime;
+
+    public GameObject EnemyPrefab;
+    public float minSpawnTime = 0;
+    public float maxSpawnTime = 0;
+    public float maxSpawnNumber = 0;
+
+	// Use this for initialization
+	void Start () {
+        enemies = new List<GameObject>();
+        nextSpawnTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if ((enemies.Count < maxSpawnNumber) && (Time.time > nextSpawnTime))
+        {
+            SpawnEnemy();
+            nextSpawnTime = Time.time + WaitTime();
+        }
+	}
+
+    private float WaitTime()
+    {
+        return Random.Range(minSpawnTime, maxSpawnTime);
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject newEnemy = Instantiate(EnemyPrefab, transform);
+        Vector3 pos = Random.onUnitSphere;
+        pos.y = .1f;
+        newEnemy.transform.localPosition = pos;
+    }
+}
