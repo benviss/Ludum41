c06239c9bd695cafa3f81fbe2cf30a472af4b5ca
Ben vissotzky <ben.vissotzky@gmail.com>
2018-04-21 17:14:36 -0700

    Camera stuff

--- Ludum41/Assets/Scripts/CameraFollow.cs
+++ Ludum41/Assets/Scripts/CameraFollow.cs
@@ -7,19 +7,39 @@ public class CameraFollow : MonoBehaviour
 
   public float smoothSpeed = 0.125f;
   public Vector3 offset;
+  public float scale = 1;
+  public float scrollSpeed;
 
   private void Start()
   {
-
+    offset = target.position - transform.position;
   }
 
   void FixedUpdate()
   {
-    Vector3 desiredPosition = target.position - offset;
-
-    Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-    transform.position = smoothedPosition;
-
+    Vector3 smoothedPosition = Vector3.Lerp(transform.position, target.position - offset, smoothSpeed);
+    transform.position = new Vector3(smoothedPosition.x, target.transform.localScale.y + 10, smoothedPosition.z);
+    //transform.RotateAround(target.position, Vector3.up, 1);
     transform.LookAt(target);
+  }
+
+  public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
+  {
+    Vector3 dir = point - pivot; // get point direction relative to pivot
+    dir = Quaternion.Euler(angles) * dir; // rotate it
+    point = dir + pivot; // calculate rotated point
+    return point; // return it
+  }
+
+  public void Scroll(float _scroll) {
+    offset += transform.forward * _scroll * scrollSpeed;
+  }
+
+  public void Rotate(float hmm)
+  {
+
+    Vector3 newOffset = RotatePointAroundPivot(transform.position, target.position, new Vector3(0,hmm*5,0));
+    offset = target.position - newOffset;
+    //transform.LookAt(target);
   }
 }
--- Ludum41/Assets/Scripts/Player.cs
+++ Ludum41/Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : LivingEntity
   public float moveSpeed = 5;
   public float attackSpeed = 2;
 
+  public CameraFollow cameraFollow;
   public GameObject attackConeObject;
   private AttackCone attackCone;
   public float lastAttack;
@@ -26,12 +27,29 @@ public class Player : LivingEntity
   {
     controller = GetComponent<PlayerController>();
     viewCamera = Camera.main;
+    cameraFollow = viewCamera.GetComponent<CameraFollow>();
   }
 
   void Update()
   {
-    // Movement input
+    // Movement inputd
     Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    float cameraRotate = 0;
+    if (Input.GetKey(KeyCode.Q)) {
+      cameraRotate = -1;
+    }
+    else if (Input.GetKey(KeyCode.E)) {
+      cameraRotate = 1;
+    } else {
+      cameraRotate = 0;
+    }
+    if (cameraRotate != 0) {
+      cameraFollow.Rotate(cameraRotate);
+    }
+    float scroll = Input.GetAxis("Mouse ScrollWheel");
+    if (scroll != 0) {
+      cameraFollow.Scroll(scroll);
+    }
     Vector3 moveVelocity = moveInput.normalized * moveSpeed;
     controller.Move(moveVelocity);
 
