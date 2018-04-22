1750edca3c7aa198728ee065cc0d91555754f511
Spencer Fisher <Fisher.spencer@gmail.com>
2018-04-22 12:29:26 -0700

    Proc City

--- Ludum41/Assets/Scripts/TileBlock.cs
+++ Ludum41/Assets/Scripts/TileBlock.cs
@@ -2,15 +2,130 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileBlock : MonoBehaviour {
+public class TileBlock : MonoBehaviour
+{
+    public LayerMask collisionMask;
+    public Building[] building;
+    public float densityLevel;
+    public float tileSize;
+    public GameObject plane;
+    public float seed;
+    public Color[] colors;
+    float maxGrassDensity = 6;
+    float minBigBuildingDEnsity = 8;
+    public EnemySpawner Spawner;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        Random.InitState((int)seed);
+        plane.transform.localScale = plane.transform.localScale * .1f * tileSize;
+        plane.GetComponent<Renderer>().material.color = GetGroundColor();
+        Spawner = Instantiate(Spawner, transform);
+        Spawner.transform.parent = transform;
+
+        for (int i = 0; i < densityLevel; i++)
+        {
+            //Debug.Log("building #"+i);
+
+            //Set to pick a random building
+            int maxBuild = building.Length;
+            if (densityLevel < minBigBuildingDEnsity)
+            {
+                maxBuild--;
+            }
+
+            int rand = Random.Range(0, maxBuild);
+
+            //Randomly places buildings throughout the map
+            Vector3 offset = new Vector3(Random.Range(tileSize * -.5f, tileSize * .5f), 0, Random.Range(tileSize * -.5f, tileSize * .5f));
+            offset = Check(offset, rand, 0);
+            offset.y = 0;
+
+            if (!IsOutOfBounds(offset))
+            {
+                Building newBuilding = Instantiate(building[rand], transform.position, Quaternion.identity);
+                newBuilding.transform.parent = transform;
+                newBuilding.transform.localPosition = offset;
+            }
+        }
+    }
+
+
+    private Vector3 Check(Vector3 offset, int buildingIndex, int attempt)
+    {
+        if (attempt > 4)
+        {
+            return (Vector3.zero);
+        }
+
+        if (offset.x > tileSize * .5f)
+        {
+            offset.x -= tileSize;
+        }
+        else if (offset.x < tileSize * -.5f)
+        {
+            offset.x += tileSize;
+        }
+
+        if (offset.z > tileSize * .5f)
+        {
+            offset.z -= tileSize;
+        }
+        else if (offset.z < tileSize * -.5f)
+        {
+            offset.z += tileSize;
+        }
+
+        Vector3 buildingSizeVector = new Vector3(building[buildingIndex].width, building[buildingIndex].length, building[buildingIndex].height);
+
+        Collider[] cols = Physics.OverlapBox(offset, buildingSizeVector * 0.5f, Quaternion.identity, collisionMask);
+
+        if (cols.Length > 0)
+        {
+            Vector3 offset2;
+            Vector3 offset3;
+            foreach (Collider c in cols)
+            {
+                offset2 = c.transform.position - offset;
+                offset2.y = 0;
+                offset3 = offset2.normalized * buildingSizeVector.magnitude * .25f;
+                offset -= offset3;
+
+                //Debug.Log("POS " + offset.ToString("F2"));
+                //Debug.Log("hit " + cols[0].transform.position.ToString("F2"));
+                //Debug.Log("moving " + offset3.ToString("F2"));
+                //Debug.Log("new pos " + (offset - offset3).ToString("F2"));
+            }
+
+            return Check(offset, buildingIndex, attempt + 1);
+        }
+
+        return offset;
+    }
+
+    private bool IsOutOfBounds(Vector3 v)
+    {
+        if (v == Vector3.zero)
+        {
+            return true;
+        }
+
+        if ((v.x > tileSize * .5 - 1) || (v.x < tileSize * -.5 + 1) ||
+            (v.z > tileSize * .5 - 1) || (v.z < tileSize * -.5 + 1))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    Color GetGroundColor()
+    {
+        if (densityLevel < maxGrassDensity)
+        {
+            return colors[0];
+        }
+        else return colors[1];
+    }
 }
---
+++ Ludum41/Assets/Scripts/TileManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+
+    public TileBlock Tile;
+    public float tileSize;
+    public int range;
+    public float seed;
+
+    public Vector2 offset = Vector2.zero;
+    public TileBlock[,] tiles;
+
+    Player player;
+	// Use this for initialization
+
+	void Start () {
+        //player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        tiles = new TileBlock[range, range];
+        AddTiles();
+        Random.InitState((int)seed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void AddTiles()
+    {
+        float scale = 5f;
+        float scale2 = 2.2f;
+        for (int y = 0; y < range; y++)
+        {
+            for (int x = 0; x < range; x++)
+            {
+                int _x = x + (int)offset.x;
+                int _y = y + (int)offset.y;
+                float result = (Mathf.PerlinNoise(_x / scale + seed, _y / scale + seed) * scale2 * tileSize);
+                result = Mathf.Pow(result, .6f);
+                Vector3 pos = new Vector3(((_x - range *.5f) * tileSize), 0, ((_y - range * .5f) * tileSize));
+
+                TileBlock newTile = Instantiate(Tile, transform);
+                newTile.transform.parent = transform;
+                newTile.transform.localPosition = pos;
+                newTile.densityLevel = result;
+                newTile.tileSize = tileSize;
+                newTile.seed = (int)Random.Range(0,10000000);
+
+                tiles[x, y] = newTile;
+            }
+        }
+    }
+}
