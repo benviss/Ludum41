62fe0d33aa4a3121d9369a7ef6d8f7abd7b71799
Ben vissotzky <ben.vissotzky@gmail.com>
2018-04-22 19:45:11 -0700

    Building explosions and stuff

--- Ludum41/Assets/Scripts/Building.cs
+++ Ludum41/Assets/Scripts/Building.cs
@@ -12,16 +12,20 @@ public class Building : LivingEntity
     public GameObject FullBuilding, DeadBuilding;
     public Vector3 location;
     public float deathTime = 1.0f;
+    public GameObject dyingBuildingEffects;
+    public GameObject deadBuildingEffects;
 
     // Use this for initialization
     void Start()
     {
+        DeadBuilding.SetActive(false);
+        base.Start();
     }
 
     public override void TakeDamage(float damage)
     {
         base.TakeDamage(damage);
-        if (health < .5f * startingHealth) {
+        if (health < .5f * startingHealth && health > 0) {
             SwapModels();
         }
     }
@@ -30,12 +34,14 @@ public class Building : LivingEntity
     {
         FullBuilding.SetActive(false);
         DeadBuilding.SetActive(true);
+        Instantiate(dyingBuildingEffects, DeadBuilding.transform);
     }
 
 
     public override void Die()
     {
+        Destroy(Instantiate(deadBuildingEffects, DeadBuilding.transform.position, DeadBuilding.transform.rotation) as GameObject,10);
+        
         Destroy(gameObject, deathTime);
-
     }
 }
